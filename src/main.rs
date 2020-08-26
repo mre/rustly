57efab8a0ccab35d377286fd52171f7a53653c83
mpw <im@mariano.pw>
2020-08-26 02:17:38 -0300

    updated dependencies, modified small things to the code to work with new versions

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,34 @@
-#![feature(plugin, custom_derive)]
-#![plugin(rocket_codegen)]
-
+#![feature(proc_macro_hygiene, decl_macro)]
+#[macro_use]
 extern crate rocket;
+
 extern crate harsh;
 
-use std::sync::RwLock;
-use rocket::State;
 use rocket::request::Form;
 use rocket::response::Redirect;
+use rocket::State;
+use std::sync::RwLock;
 
 mod repository;
 mod shortener;
 use repository::Repository;
 
-#[derive(FromForm)]
+#[derive(FromForm, Debug)]
 struct Url {
     url: String,
 }
 
-
 #[get("/<id>")]
-fn lookup(repo: State<RwLock<Repository>>, id: &str) -> Result<Redirect, &'static str> {
-    match repo.read().unwrap().lookup(id) {
-        Some(url) => Ok(Redirect::permanent(url)),
-        _ => Err("Requested ID was not found.")
+fn lookup(repo: State<RwLock<Repository>>, id: String) -> Result<Redirect, &'static str> {
+    match repo.read().unwrap().lookup(&id) {
+        Some(url) => Ok(Redirect::permanent(format!("{}", url))),
+        _ => Err("Requested ID was not found."),
     }
 }
 
 #[post("/", data = "<url_form>")]
 fn shorten(repo: State<RwLock<Repository>>, url_form: Form<Url>) -> Result<String, String> {
-    let ref url = url_form.get().url;
+    let ref url = format!("{}", url_form.0.url);
     if !url.starts_with("https") && !url.starts_with("http") {
         return Err(format!("Not a valid URL {:?}", url));
     }
@@ -39,7 +38,8 @@ fn shorten(repo: State<RwLock<Repository>>, url_form: Form<Url>) -> Result<Strin
 }
 
 fn main() {
-    rocket::ignite().manage(RwLock::new(Repository::new()))
-                    .mount("/", routes![lookup, shorten])
-                    .launch();
+    rocket::ignite()
+        .manage(RwLock::new(Repository::new()))
+        .mount("/", routes![lookup, shorten])
+        .launch();
 }
